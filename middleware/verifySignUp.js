8c76bffd3a5406943b13f0ba9ab366994a2901e8
gmalik83 <gaurav98malik@gmail.com>
2022-04-08 22:06:36 +0530

    Changed for tempdata

--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -3,12 +3,12 @@
 const db = require('../models/index');
 const ROLES = db.ROLES;
 const TempData = db.tempData;
-const Blacklist = db.blacklist;
-const Volunteer = db.volunteer;
+// const Blacklist = db.blacklist;
+// const Volunteer = db.volunteer;
 
 checkDuplicateEmail = (req, res, next) => {
   // Check for Email in Blacklist
-  Blacklist.findOne({ email: req.body.email }).exec((err, user) => {
+  /*Blacklist.findOne({ email: req.body.email }).exec((err, user) => {
     // If there is any Error present
     if (err) {
       res.status(500).send({ message: err });
@@ -19,23 +19,22 @@ checkDuplicateEmail = (req, res, next) => {
       res.status(400).send({ message: 'You have been Blacklisted.' });
       return;
     }
+    */
 
-    // Check for Email in TempData
-    TempData.findOne({ email: req.body.email }).exec((err, user) => {
-      // If Any Error Present
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-      // If User Exists in Temp Database
-      if (user) {
-        res
-          .status(400)
-          .send({ message: 'Your Request is Already Under Process' });
-        return;
-      }
+  // Check for Email in TempData
+  TempData.findOne({ email: req.body.email }).exec((err, user) => {
+    // If Any Error Present
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    // If User Exists in Temp Database
+    if (user) {
+      res.status(400).send({ message: 'You have already submitted. Line 35' });
+      return;
+    }
 
-      // Check for EMail in Volunteer Database
+    /*  // Check for EMail in Volunteer Database
       Volunteer.findOne({ email: req.body.email }).exec((err, user) => {
         // If There is Error Present
         if (err) {
@@ -48,11 +47,12 @@ checkDuplicateEmail = (req, res, next) => {
           res.status(400).send({ message: 'Already Registered' });
           return;
         }
+        */
 
-        next();
-      });
-    });
+    next();
   });
+  // });
+  // });
 };
 // Check if Roles Exist
 checkRoleExisted = (req, res, next) => {
